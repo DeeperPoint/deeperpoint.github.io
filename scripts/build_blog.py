39107c99604a503367149c2778dc8013d09058b5
uzumeri <vic.uzumeri@ipov.net>
2026-05-11 11:00:02 -0400

    FEAT: restore Ebook link to site-wide navigation

--- scripts/build_blog.py
+++ scripts/build_blog.py
@@ -354,6 +354,7 @@ PAGE_HEAD = """<!-- Copyright (c) 2026 Mustafa Uzumeri. All rights reserved. -->
         <li><a href="{root}index.html" class="nav__link">Home</a></li>
         <li><a href="{root}thin-markets.html" class="nav__link">Theory</a></li>
         <li><a href="{root}intervention-matrix.html" class="nav__link">Matrix</a></li>
+        <li><a href="{root}ebook.html" class="nav__link">Ebook</a></li>
         <li><a href="{root}catalog/index.html" class="nav__link">Catalog</a></li>
         <li><a href="{root}marketmaps.html" class="nav__link">MarketMaps</a></li>
         <li><a href="{root}cosolvent.html" class="nav__link">Cosolvent</a></li>
